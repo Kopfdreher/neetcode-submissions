74fe6aba107aeb5aa5b7cd28a83c78dd406dd855
Sergej Gavrilov <167129187+Kopfdreher@users.noreply.github.com>
2026-09-01 12:56:17 +0200

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string result;
+
+        for (string str : strs) {
+            result += to_string(str.length());
+            result += "#";
+            result += str;
+        }
+
+        return result;
+    }
+
+    vector<string> decode(string s) {
+        int end = 0;
+        int start = 0;
+        int len = 0;
+        vector<string> result;
+        while (1) {
+            end = s.find('#', start);
+            if (end == string::npos) break;
+            len = stoi(s.substr(start, end - start));
+            result.push_back(s.substr(end + 1, len));
+            start = end + 1 + len;
+        }
+        return result;
+    }
+};
