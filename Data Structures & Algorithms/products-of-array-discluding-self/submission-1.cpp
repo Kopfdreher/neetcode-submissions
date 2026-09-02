d99c7128ee87fd5ff1446ee7ba9c2a42f0ab95b4
Sergej Gavrilov <167129187+Kopfdreher@users.noreply.github.com>
2026-09-02 15:37:52 +0200

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cpp
@@ -0,0 +1,27 @@
+auto init = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int len = nums.size();
+        vector<pair<int,int>> products(len);
+        vector<int> result(len);
+
+        products[0].first = 1;
+        products[len - 1].second = 1;
+
+        for (int i = 1; i < len; i++) {
+            products[i].first = nums[i - 1] * products[i - 1].first;
+            products[len - i - 1].second = nums[len - i] * products[len - i].second;
+        }
+
+        for (int i = 0; i < len; i++) {
+            result[i] = products[i].first * products[i].second;
+        }
+        return result;
+    }
+};
