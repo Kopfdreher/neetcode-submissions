39f36a4ae0ab7540f6b26eda595f54449101aa8a
Sergej Gavrilov <167129187+Kopfdreher@users.noreply.github.com>
2026-09-05 13:18:13 +0200

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.size() - 1;
+
+        while (i < s.size() && j > 0 && i < j) {
+            while (i < s.size() && !isalnum(s[i])) {
+                i++;
+            }
+            while (j > 0 && !isalnum(s[j])) {
+                j--;
+            }
+
+            if (i >= s.size() || j < 0) break;
+
+            if (tolower(s[i]) != tolower(s[j])) return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
