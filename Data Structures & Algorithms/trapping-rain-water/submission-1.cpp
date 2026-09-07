120dec104e7e450186049cee5d653e012491d400
Sergej Gavrilov <167129187+Kopfdreher@users.noreply.github.com>
2026-09-07 13:03:02 +0200

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.cpp
@@ -0,0 +1,28 @@
+static const auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    int trap(vector<int>& h) {
+        vector<int> prefix(h.size(), 0);
+        int rtn = 0;
+        int suffix = 0;
+        
+        for (int i = 1; i < h.size(); ++i) {
+            prefix[i] = max(h[i - 1], prefix[i - 1]);
+        }
+
+        for (int i = h.size() - 2; i > 0; --i) {
+            suffix = max(suffix, h[i + 1]);
+            //printf("suffix = %i\n", suffix);
+            //printf("prefix[i] = %i\n", prefix[i]);
+            int trapped = min(prefix[i], suffix) - h[i] ;
+            //printf("trapped = %i\n", trapped);
+            if (trapped > 0) rtn += trapped;
+        }
+        return rtn;
+    }
+};
