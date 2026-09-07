9be53c67a2b5a449ba3f57045a2f7a09e4aefc65
Sergej Gavrilov <167129187+Kopfdreher@users.noreply.github.com>
2026-09-07 12:00:39 +0200

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.cpp
@@ -0,0 +1,39 @@
+#pragma optimize("OFast","unroll-loops");
+
+static const auto fast = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+   public:
+    vector<vector<int>> threeSum(vector<int>& n) {
+        sort(n.begin(), n.end());
+        unsigned int i = 0;
+        vector<vector<int>> rtn;
+        while (i < n.size() - 2) {
+            if (i > 0 && n[i] == n[i - 1]) {
+                i++;
+                continue;
+            }
+            unsigned int j = i + 1;
+            unsigned int k = n.size() - 1;
+            while (j < k) {
+                if (n[i] + n[j] + n[k] > 0)
+                    k--;
+                else if (n[i] + n[j] + n[k] < 0)
+                    j++;
+                else {
+                    rtn.emplace_back(vector<int>{n[i], n[j], n[k]});
+                    j++;
+                    k--;
+                    while (j < k && n[j] == n[j - 1]) j++;
+                    while (j < k && n[k] == n[k + 1]) k--;
+                }
+            }
+            i++;
+        }
+        return (rtn);
+    }
+};
