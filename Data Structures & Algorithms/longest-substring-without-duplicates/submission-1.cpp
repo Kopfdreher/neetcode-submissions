7343e03c1d3bf069d17da313f06dd6bc4b1dec5e
Sergej Gavrilov <167129187+Kopfdreher@users.noreply.github.com>
2026-09-08 10:30:41 +0200

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> hash(256, -1);
+        int l = 0, r = 0, maxS = 0;
+
+        while (r < s.size()) {
+            if (hash[s[r]] >= l) l = hash[s[r]] + 1;
+            hash[s[r]] = r;
+            r++;
+            maxS = max(maxS, r - l);
+        }
+        return maxS;
+    }
+};
